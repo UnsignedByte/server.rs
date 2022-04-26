22f19acd08d93d0740a14cb0c2a72ea19d387b78
UnsignedByte <edlam1889@gmail.com>
2022-04-26 09:29:48 -0700

    size

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use std::path::Path;
 pub use rocket;
 pub use rocket_contrib;
 
-pub trait Launch {
+pub trait Launch : Sized {
     /// Create a new launcher on a root path `root`.
-    fn new <P: AsRef<Path>> (root: P) -> Self;
+    fn new <P: AsRef<Path>> (root: P) -> std::io::Result<Self>;
 
     /// mount the launcher onto an app `app` at subpath `path` (consuming).
     fn mount(self, path: &str, app: rocket::Rocket) -> rocket::Rocket;
