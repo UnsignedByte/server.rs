0e5f7a5df60b73a13418ecb90030b2d11926eb3f
UnsignedByte <edlam1889@gmail.com>
2022-04-26 09:21:33 -0700

    update signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use std::path::Path;
 pub use rocket;
 pub use rocket_contrib;
 
 pub trait Launch {
-    fn new() -> Self;
+    /// Create a new launcher on a root path `root`.
+    fn new <P: AsRef<Path>> (root: P) -> Self;
 
+    /// mount the launcher onto an app `app` at subpath `path` (consuming).
     fn mount(self, path: &str, app: rocket::Rocket) -> rocket::Rocket;
 }
